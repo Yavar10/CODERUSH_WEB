d4b4aaaf2bf10f55cc5570e815b89b66bcf4d76e
Yavar <zuri00014@gmail.com>
2026-08-13 14:29:46 +0530

    brochure route added

--- switch_website/next.config.mjs
+++ switch_website/next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
           "https://unstop.com/competitions/code-rush-30-krishna-institute-of-engineering-and-technology-kiet-ghaziabad-1731472",
         permanent: false,
       },
+      {
+            source: "/brochure",
+        destination:
+          "https://drive.google.com/file/d/1A_jveOONrVJLTWkGk3oqMJJjSTWQiYja/view?usp=drivesdk",
+        permanent: false,
+      },
     ];
   },
 };
