e5ba0e05bf82dc2a4b40a80e2722660a6c4c364b
Yavar <zuri00014@gmail.com>
2026-08-14 11:50:34 +0530

    feat: fixed the scroll bug

--- switch_website/src/components/SmoothScroll.tsx
+++ switch_website/src/components/SmoothScroll.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -11,20 +11,23 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
 
     const lenis = new Lenis({
       smoothWheel: true,
+      wheelMultiplier: 1,
     });
 
     lenis.on("scroll", ScrollTrigger.update);
 
-    const updateRaf = (time: number) => {
-      lenis.raf(time);
-      requestAnimationFrame(updateRaf);
-    };
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
 
-    const rafId = requestAnimationFrame(updateRaf);
+    gsap.ticker.lagSmoothing(0);
 
     return () => {
-      cancelAnimationFrame(rafId);
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000);
+      });
       lenis.destroy();
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, []);
 
